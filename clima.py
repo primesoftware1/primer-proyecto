42ac1f2cc0052099eb83b2ffd05490fcabd61648
primesoftware1 <abregoangel181@gmail.com>
2025-12-11 17:55:16 -0500

    Add files via upload

---
+++ clima.py
@@ -0,0 +1,9 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> temperatura = float(input("Ingresa la temperatura en grados: "))
+... 
+... if temperatura < 15:
+...     print("Hace frío.")
+... elif temperatura <= 28:
+...     print("El clima es agradable.")
+... else:
